9242026f73d14a420b88124d816bfbb95e957af0
EddEnt <61289536+EddEnt@users.noreply.github.com>
2023-02-19 13:27:06 -0500

    Updated resolvers/index.js
    Code comment for resolvers index file.

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -1,3 +1,9 @@
+/**
+ * Not the main index.js file, but the one in the resolvers folder
+ * This file is used to combine all the resolvers into one file,
+ * then imported into the main index.js file, and used to call our resolvers.
+ */
+
 const usersResolvers = require("./Users");
 const employeesResolvers = require("./Employees");
 
